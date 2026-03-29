18b8ddbe5085b3396e11799df147ffac0b1b5e37
Gourab-coder <gourabdas034@gmail.com>
2026-03-30 04:47:21 +0530

    DONE

--- server/app/config/db.js
+++ server/app/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT, // 🔥 IMPORTANT
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT, // 🔥 IMPORTANT
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 
   waitForConnections: true,
   connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
